cbaba0d251d402a2f86ff1fda6ab6316ee88aa4a
Matthew Brooks <mdeanbrooks@gmail.com>
2016-07-03 14:44:00 -0700

    begin fleshing out main editor components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import MainEditor from './MainEditor';
 
 export default class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = { text: '' }
+  }
+  render () {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>今は何をしている？</h1>
+        <section>
+          <MainEditor />
+        </section>
+        <section>
+          {this.state.text}
+        </section>
+      </div>
     );
   }
 }
