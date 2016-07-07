0edd83cb6f406e4b8e562a30246dbce239491a34
Matthew Brooks <mdeanbrooks@gmail.com>
2016-07-06 23:11:38 -0700

    add initial redux setup boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
